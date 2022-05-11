a0c6a2543e3806f31166419133b62da28e04ff8a
Rafheros <rafaelheros80@gmail.com>
2022-05-11 00:47:17 -0300

    Working app

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionsTable";
+import { Container } from "./styles";
+
+
+export function Dashboard () {
+    return (
+        <>
+            <Container>
+                <Summary/>
+                <TransactionTable/>
+            </Container>
+
+        </>
+    );
+}
